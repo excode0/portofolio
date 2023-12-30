991048f35aa05389fe034e14f7b7c5937d0ff9f2
muhammadrivan3 <oyenit888@gmail.com>
2023-12-31 06:37:14 +0700

    portofolio

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,6 @@ import {
 } from "./components";
 
 const App = () => {
-  const [animationDelays, setAnimationDelays] = useState([]);
-  const [animationPositions, setAnimationPositions] = useState([]);
-
-  const getRandomDelay = () => Math.floor(Math.random() * 8) + 5;
-
-  const getRandomLeft = () => Math.floor(Math.random() * 101);
-
-  const getRandomBottom = () => Math.floor(Math.random() * 141) - 20;
-
-  const getRandomTransform = () =>
-    `translateX(${Math.random() * 100}px) translateY(${Math.random() * 100}px)`;
-
-  useEffect(() => {
-    const numElements = 1800;
-    const delays = Array.from({ length: numElements }, () => getRandomDelay());
-    const positions = Array.from({ length: numElements }, () => ({
-      left: getRandomLeft(),
-      bottom: getRandomBottom(),
-    }));
-    setAnimationDelays(delays);
-    setAnimationPositions(positions);
-  }, []);
-
   return (
     <>
       <BrowserRouter>
